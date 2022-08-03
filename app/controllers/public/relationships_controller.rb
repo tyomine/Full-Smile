24f009b334590e94704810414f5781cd187bf00b
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-08-03 08:06:22 +0000

    コントローラー、ルーティング作成

---
+++ app/controllers/public/relationships_controller.rb
@@ -0,0 +1,14 @@
+class Public::RelationshipsController < ApplicationController
+  
+  def cretate
+  end
+  
+  def destroy
+  end
+  
+  def followings
+  end
+
+  def followers
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,31 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+  
+  scope module: :public do
+    root to: "homes#top"
+    get '/about' => 'homes#about'
+    
+    resources :users, only: [:show, :edit, :update] do
+      resource :relationships, only: [:create, :destroy]
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers' => 'relationships#followers', as: 'followers'
+    end
+    
+    resources :posts, only: [:new, :create, :show, :index, :edit, :update, :destroy] do
+      resource :likes, only: [:create, :destroy]
+      get 'posts/likes' => "likes#index"
+      resources :comments, only: [:create, :destroy]
+    end
+  end  
+  
+  
+  namespace :admin do
+    root to: "homes#top"
+    
+    resources :users, only: [:index, :show]
+    resources :posts, only: [:show]
+    
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
