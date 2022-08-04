53ee8c224e5e70dd4cb5e2d8cc19bf6b3afb8620
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-08-04 08:04:07 +0000

    新規投稿、投稿詳細ページ、投稿一覧ページ作成

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,13 +1,40 @@
 class Public::PostsController < ApplicationController
+  
   def new
+    @post = Post.new
+    
   end
+  
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to post_path(@post)
+  end  
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def index
+    @post = Post.all
+    @user = current_user
   end
 
   def edit
   end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :body, :image)
+  end
+  
+  
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,7 +1,12 @@
 class Public::UsersController < ApplicationController
+  
   def show
+    @user = User.find(params[:id])
+    @post = @user.posts
   end
 
   def edit
   end
+  
+  
 end
