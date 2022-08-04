548fbe7115b474bddf5de540299d48d824442e5f
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-08-04 09:10:42 +0000

    削除機能追加

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -32,6 +32,9 @@ class Public::PostsController < ApplicationController
   end
   
   def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
   end
   
   private
