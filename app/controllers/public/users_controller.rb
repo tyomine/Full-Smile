ce51e8d085bde2c4bd5f1e67d33b9e36f748426e
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-08-04 15:09:00 +0000

    ユーザー一覧、詳細ページ、編集機能追加

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -2,11 +2,22 @@ class Public::UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    @post = @user.posts
+    @posts = @user.posts
   end
 
   def edit
+    @user = User.find(params[:id])
   end
   
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
   
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
 end
