cb94419c4a1c5da2945474ead8709260f6d52ce8
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-08-11 03:10:45 +0000

    admin側投稿一覧画面レイアウト遷移先編集

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,6 +2,6 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
   
   def top
-    @posts = Post.all
+    @posts = Post.page(params[:page]).order(created_at: :desc)
   end
 end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -4,7 +4,7 @@ class Public::SessionsController < Devise::SessionsController
   #before_action :configure_sign_in_params, only: [:create]
   
   def after_sign_in_path_for(resource)
-   posts_path
+   user_path(@user.id)
   end
 
   # GET /resource/sign_in
