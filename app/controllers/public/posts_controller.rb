8ba82f58c86180f4704966bc2751ce0cee004d46
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-08-11 01:21:16 +0000

    レイアウト編集（admin,public）ログイン画面

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -44,7 +44,7 @@ class Public::PostsController < ApplicationController
   def destroy
     post = Post.find(params[:id])
     post.destroy
-    redirect_to posts_path, notice: "削除が完了しました。"
+    redirect_to posts_path, notice: "削除しました。"
   end
   
   private
