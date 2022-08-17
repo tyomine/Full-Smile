2c027c7beac99179104eb08ee246e2213d9f975f
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-08-17 11:37:38 +0000

    ページネーション設定

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -3,7 +3,7 @@ class Public::NotificationsController < ApplicationController
   
   def index
     #current_userの投稿に紐づいた通知一覧
-    @notifications = current_user.passive_notifications
+    @notifications = current_user.passive_notifications.page(params[:page]).order(created_at: :desc)
     # @notificationの中でまだ確認していない(indexに一度も遷移していない)通知のみ
     @notifications.where(checked: false).each do |notification|
       notification.update(checked: true)
