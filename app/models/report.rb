ea286b0c02006b1827d4ca7db17ade97f0ee96da
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-08-20 00:43:06 +0000

    コメント名前の遷移先変更

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,6 +3,8 @@ class Report < ApplicationRecord
   belongs_to :reporter, class_name: "User"
   belongs_to :reported, class_name: "User"
   
+  validates :reason, presence: true
+  
   enum status: { waiting: 0, keep: 1, finish: 2 }
   
 end
